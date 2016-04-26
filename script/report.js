1b0f82daa0b8d93bc1ebbf69eaf1b3d003d4f04d
tuierbufei <52435770@qq.com>
2016-04-26 18:35:41 +0800

    Update report.js

--- script/report.js
+++ script/report.js
@@ -5,42 +5,57 @@
 function createReport(panel, stkcd, type, callBack) {
     panel.empty();
     $.getJSON("http://query.yahooapis.com/v1/public/yql", {
-        q: 'select * from json where url=\"http://basic.10jqka.com.cn/'+ stkcd +'/flash/' + type + '.txt\"',
+        q: 'select * from json where url=\"http://basic.10jqka.com.cn/' + stkcd + '/flash/' + type + '.txt\"',
         format: "json"
     }, function (data) {
         if (data.query.results) {
             var titles = data.query.results.json['title'],
                 select = $('<select>'),
-                option = {'year': '按年报期', 'report': '按报告期', 'simple': '按单季度期'};
-            for(var key in option) {
-                $("<option />", {value: key, text: option[key]}).appendTo(select);
+                option = {
+                    'year': '按年报期',
+                    'report': '按报告期',
+                    'simple': '按单季度期'
+                };
+            for (var key in option) {
+                $("<option />", {
+                    value: key,
+                    text: option[key]
+                }).appendTo(select);
             }
             panel.append(select);
-            $.each(data.query.results.json, function(key) {
-                if(key != 'title') {
-                    var table = $('<table>'),
+            $.each(data.query.results.json, function (key) {
+                if (key != 'title') {
+                    var table = $('<table class="table">'),
                         container = $('<div>'),
                         tRow,
                         tCell;
                     table.attr('id', key);
                     container.addClass('fincial-table-container');
 
-                    $.each(this, function(i){
+                    $.each(this, function (i) {
                         tRow = $('<tr>');
                         var titleLabel = '';
-                        if(titles[i].json && (titles[i].json instanceof Array)) {
-                            for(var j = 0; j< titles[i].json.length; j++) {
-                                titleLabel += j>0 && titles[i].json[j] != '' ? ('(' + titles[i].json[j] +')') :titles[i].json[j];
+                        if (titles[i].json && (titles[i].json instanceof Array)) {
+                            for (var j = 0; j < titles[i].json.length; j++) {
+                                titleLabel += j > 0 && titles[i].json[j] != '' ? ('(' + titles[i].json[j] + ')') : titles[i].json[j];
                             }
                         } else {
                             titleLabel = titles[i];
                         }
-                        tRow.append($('<td>').html(titleLabel));
-                        $.each(this.json, function(j){
-                            tCell = $('<td>').html(this);
+                        tRow.append($('<th style="width:20%">').html(titleLabel));
+                        $.each(this.json, function (j) {
+                            if (i == 0) {
+                                tCell = $('<th>').html(this);
+                            } else {
+                                tCell = $('<td>').html(this)
+                            }
                             tRow.append(tCell)
                         });
-                        table.append(tRow);
+                        if (i == 0) {
+                            table.append($('<thead>').append(tRow));
+                        } else {
+                            table.append(tRow);
+                        }
                     });
 
                     container.append(table);
@@ -48,11 +63,12 @@ function createReport(panel, stkcd, type, callBack) {
                 }
             });
 
-            select.change(function(e){
-                var select = e.target, tables = panel.find('table');
-                $.each(tables, function(i){
+            select.change(function (e) {
+                var select = e.target,
+                    tables = panel.find('table');
+                $.each(tables, function (i) {
                     $(this).hide();
-                    if($(this).attr('id') == $(select).find("option:selected")[0].value) {
+                    if ($(this).attr('id') == $(select).find("option:selected")[0].value) {
                         $(this).show();
                     }
                 })
