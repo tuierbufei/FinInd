b192295ec9b0833a4b0ccd95f4140619dab3b984
tuierbufei <52435770@qq.com>
2016-06-10 16:55:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -56,42 +56,4 @@ requirejs.config({
     }
 });
 
-// Start loading the main app file.
-requirejs(['jquery','domready','app/main'], function($, domready) {
-    var customIconNavTab = ['companyNav', 'industryNav'];
-    domready(function () {
-        var navs = $('#navTab').find('a');
-        var activeTab = $('#navTab').find('.active')[0];
-        if(activeTab != undefined && customIconNavTab.indexOf(activeTab.id) != -1) {
-            var icon = $(activeTab).find('img')[0];
-            if(icon != undefined) {
-                icon.src = $(icon).attr('data-selected');
-            }
-        }
-
-        $.each(navs, function(index, tab){
-            //navs.find('.active').
-            $(tab).on('click', function(e){
-                var previous = $(e.currentTarget).parent().find('.active')[0];
-                if(previous != undefined && customIconNavTab.indexOf(previous.id) != -1) {
-                    var icon = $(previous).find('img')[0];
-                    if(icon != undefined) {
-                        icon.src = $(icon).attr('data-unselected');
-                    }
-                }
-
-                if(customIconNavTab.indexOf(e.currentTarget.id) != -1) {
-                    var icon = $(e.currentTarget).find('img')[0];
-                    if(icon != undefined) {
-                        icon.src = $(icon).attr('data-selected');
-                    }
-                }
-            });
-
-            var icon = $(tab).find('img')[0];
-            if(icon != undefined && icon.src == '') {
-                icon.src = $(icon).attr('data-unselected');
-            }
-        });
-    });
-});
+requirejs(['app/main']);
